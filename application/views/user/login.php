3f2de11914c36c97d2c29afb07c10772e70052b2
Selgueda <selgueda@ug.uchile.cl>
2014-06-16 04:20:58 -0400

    Estilos al login

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -1,32 +1,37 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>TravelSyst - Login</title>
-	</head>
-	<body>
-		<?php
-			if(isset($error) && $error == TRUE){
-				echo "El usuario no ha sido encontrado.<br>";
-			}
-		?>
-		Logueese por favor.<br><br>
-		<?php
-			echo form_open('User/checkLogin')."\n";
-			echo form_label('Usuario', 'usuario')."\n";
-			echo form_input('username')."\n";
-			echo form_label('Password', 'pass')."\n";
-			echo form_password('password')."\n";
-		
+<?php
+	if(isset($error) && $error == TRUE){
+		echo "El usuario no ha sido encontrado.<br>";
+	}
+?>
+<div class="row">
+	<?php echo form_open('User/checkLogin')."\n"; ?>
+	<div class="col-sm-6 col-sm-offset-3">
+		<h2>Iniciar Sesión</h2>
+		<div class="group-control">
+			<?php echo form_label('Usuario', 'usuario')."\n"; ?>
+			<?php echo form_input(array('class' => 'form-control','name' => 'username'))."\n"; ?>			
+		</div>	
+		<br>
+		<div class="group-control">
+			<?php echo form_label('Contraseña', 'pass')."\n"; ?>
+			<?php echo form_password(array('class' => 'form-control','name' => 'password'))."\n"; ?>			
+		</div>
+		<br>
+		<div class="row">
+			<div class="col-sm-10">
+				<?php echo form_submit(array('value' => 'Iniciar Sesión','name' =>'send','class' => 'btn btn-info' ) ); ?>
+				<?php echo form_close(); ?>			
+			</div>
+			<div class="col-sm-2">
+				<?php echo anchor('User/register', 'Registrar'); ?>			
+			</div>				
+		</div>
 	
-			echo form_submit('send', 'Log in');
-			echo form_close();
+	
+	</div>
 
-			echo "<br>";
-			echo anchor('User/register', 'Registrar');
-		?>
-	</body>
-</html>
+</div>		
+		
 
 
 	
